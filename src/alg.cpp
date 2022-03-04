d6731b9aa01ce87e9c0d1a7d8145254d8af7382d
EgorShanin <94217235+EgorShanin@users.noreply.github.com>
2022-03-04 14:17:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,10 +5,10 @@
 
 double pown(double value, uint16_t n) {
     double pow = value;
-    if(n == 0) {
+    if (n == 0) {
         return 1;
     } else {
-        for(uint64_t i = 1; i < n; i++) {
+        for (uint64_t i = 1; i < n; i++) {
             value = (value * pow);
         }
         return value;
@@ -17,7 +17,7 @@ double pown(double value, uint16_t n) {
 
 uint64_t fact(uint16_t n) {
     uint64_t f = 1;
-    for(uint64_t i = 1; i <= n; i++) {
+    for (uint64_t i = 1; i <= n; i++) {
         f = (f * i);
     }
     return f;
@@ -32,7 +32,7 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
     double e = 0;
-    for(uint64_t i = 0; i <= count; i++) {
+    for (uint64_t i = 0; i <= count; i++) {
         e += calcItem(x, i);
     }
     return e;
@@ -40,7 +40,7 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
     double s = 0;
-    for(uint64_t i = 1; i <= count; i++) {
+    for (uint64_t i = 1; i <= count; i++) {
         s += (pown(-1, i-1) * calcItem(x, ((2 * i) - 1)));
     }
     return s;
@@ -48,7 +48,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
     double c = 0;
-    for(uint64_t i = 1; i <= count; i++) {
+    for (uint64_t i = 1; i <= count; i++) {
         c += (pown(-1, i-1) * calcItem(x, ((2 * i) - 2)));
     }
     return c;
